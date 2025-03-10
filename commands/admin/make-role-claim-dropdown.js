05a15517d58d013d0951fd40ea4f98b84869d233
thekightkeeper1 <ty.singh828@gmail.com>
2025-03-10 11:35:47 -0400

    todo: fix error with multiline csv

--- commands/admin/make-role-claim-dropdown.js
+++ commands/admin/make-role-claim-dropdown.js
@@ -18,95 +18,95 @@ module.exports = {
             option
                 .setName('template_name')
                 .setDescription("Make dropdowns from a template you have uploaded before.")
+                .setRequired(true)
         )
         .addAttachmentOption(option =>
             option
                 .setName("template_file")
                 .setDescription("Upload and save a new template, using it to generate the dropdowns.")
         ),
+
+
     execute: async (interaction) => {
 
         await interaction.deferReply({
             flags: MessageFlags.Ephemeral
         });
 
+        const guildId = interaction.guildId;
+        const templateName = interaction.options.getString("template_name");
+        const templateFolder = `database/${guildId}/make-role-claim-dropdown/templates`;
+        if (!fs.existsSync(templateFolder)) {  //todo if it doesnt exist maybe we should make it?
+            console.log("The template folder did not exist");
+            interaction.followUp({
+                content: "Error: Template folder does not exist. Ask the dev to (re)register your server",
+            })
+        }
+
+        const templateSaveDir = path.join(templateFolder, templateName + ".json");
+
         // Seeing if sent us a new template or if we use an existing
-        let templateName = "";
-        let categories;
         let components;
-
-        // In this case we use the file they uploaded, parse it, then save it.
-        if (interaction.options.getAttachment("template_file")) {  //todo ensure we didnt specify both template_file and template_name
+        if (interaction.options.getAttachment("template_file")) {
+            // In this case we use the file they uploaded, parse it, then save it.
             // Fetch the file
-            const url = interaction.options.getAttachment("template_file").url;
-            const templateText = await getTextAttachment(url);
+            const attachmentURL = interaction.options.getAttachment("template_file").url;
+            const templateText = await getTextAttachment(attachmentURL);
 
-            /* Saving the file */
-            // First ensuring the folder to save to exists
-            const guildId = interaction.guildId;
-            const templateFolder = `database/${guildId}/make-role-claim-dropdown/templates`;
-            if (!fs.existsSync(templateFolder)) {  //todo if it doesnt exist maybe we should make it?
-                console.log("The template folder did not exist");
-                interaction.editReply({
-                    content: "Error: Template folder does not exist",
-                })
-                console.error("Template folder did not exist")
+
+            // parsing the template
+            components = parseCSV(templateText, interaction)
+            if (!components) {
+                interaction.followUp({
+                    content: "I ran into an issue parsing the csv.",
+                    flags: MessageFlags.Ephemeral
+                });
+                return;
             }
 
-            // Getting the name of the template from the uploaded attachment
-            templateName = templateText.split('\n')[0] + '.txt';
-            const templateFP = path.join(process.cwd(), templateFolder, templateName);
-            fs.writeFileSync(templateFP, templateText, 'utf8');
-            /*
-            * template_name
-            * multiselect_t/f,category1,opt1,opt2
-            * multiselect_t/f,category2,opt1,opt2
-            * */
+            fs.writeFile(templateSaveDir, JSON.stringify(components), (err) => {
+                if (!err) return;
+                console.error(err);
+                interaction.followUp({
+                    content: "There was an issue saving the file to my database. Please contact the developer.",
+                    flags: MessageFlags.Ephemeral
+                })
+            })
+            return;
         }
 
         /* Loading the template file*/
         if (!components) { // If we uploaded a template then this would be defined
-            // Reading the template text from the server filesystem
-            // Todo make a database code?
-            // Loading the file
-            templateName = interaction.options.getString("template_name");
-            const guildId = interaction.guildId;
-            const templateFP = path.join(
-                process.cwd(),
-                'database',
-                guildId,
-                'make-role-claim-dropdown',
-                "templates",
-                templateName + ".csv"
-            )
 
-            if (!fs.existsSync(templateFP)) {
+            // Loading template
+            if (!fs.existsSync(templateSaveDir)) {
                 interaction.editReply("I didn't find a template with that name.");
-                console.log("There was no template file.");
+                console.log(`There was no template file ${templateName}.`);
             }
 
             // Parsing the csv
-            const templateText = fs.readFileSync(templateFP, 'utf8');
-            const categories = parseCSV(templateText, interaction);
-
-            components = categories.map(e => new ActionRowBuilder().addComponents([
-                new StringSelectMenuBuilder()
-                    .setCustomId(uuidv4())
-                    .setPlaceholder(e.category)
-                    .setMinValues(Number(e.minValues))
-                    .setMaxValues(Number(e.maxValues))
-                    .setOptions(e.options.map(role => new SelectMenuOptionBuilder()
-                        .setValue(role.id)
-                        .setLabel(role.name)
-                    ))
-            ]))
-
+            components = JSON.parse(fs.readFileSync(templateSaveDir, 'utf8'));
         }
+
+        actionRow[0] = (new StringSelectMenuBuilder()
+                .setCustomId("replace_this_when_executing")
+                .setPlaceholder(record[columns.placeholder])
+                .setMaxValues(+record[columns.maxValues])
+                .setMinValues(+record[columns.minValues])
+        )
+
+        return;
+        // Generating uuid for the custom id
+        components = components.map(actionRow => new ActionRowBuilder()
+            .addComponents([
+                new StringSelectMenuBuilder(actionRow.components[0])
+                    .setCustomId(uuidv4())
+            ])
+        )
 
         interaction.channel.send({
             content: "Choose roles below.",
             components: components,
-
         })
     }
 }
@@ -115,38 +115,66 @@ module.exports = {
 function parseCSV(csvString, interaction) {
     let csvRows = csvString.split('\n');
     csvRows = csvRows.map(s => s.trim().split(','))
-    csvRows = csvRows.slice(1)
+
+    // Gets indices of column
+    const columns = {
+        maxValues: "MAX_OPT",
+        minValues: "MIN_OPT",
+        placeholder: "CAT_NAME",
+        roles: "ROLES"
+    };
+
+    Object.keys(columns).forEach(key => {
+        columns[key] = csvRows[0].indexOf(columns[key]);
+    });
+    csvRows = csvRows.slice(1);
+
 
     let allRolesExist = true;
-    const categories = []
-    // Object mirroring a string select menu
+    let errors = "These roles were not found in your server:";
+    const components = [];  // This is what is sent in interaction.reply({components: components})
     csvRows.forEach(record => {
-        if (record.length <= 1) return;
-        categories.push({
-            category: record[2],
-            minValues: record[0],
-            maxValues: record[1],
-            options: record.slice(3).map((s) => {
-                    // Gets the role ids and outputs errors to the interaction
+        if ("" === record[0]) return;
+        if (record.length < columns.roles + 1) {
+            interaction.followUp({
+                content: "This row didn't have at least" + columns.roles + " items in it:" +
+                    "\n```js\n" + record + "\n```"
+
+            })
+        }
+        components.push(new ActionRowBuilder().addComponents([
+            new StringSelectMenuBuilder()
+                .setCustomId("replace_this_when_executing")
+                .setPlaceholder(record[columns.placeholder])
+                .setMaxValues(+record[columns.maxValues])
+                .setMinValues(+record[columns.minValues])
+                .setOptions(record.slice(columns.roles).map(s => {
+                    if (s === "") return;
                     const role = interaction.guild.roles.cache.find(r => s === r.name || s === r.id);  // r is a role
                     if (!role) {
+                        errors += `category: ${record[2]} role: ${s}`;
                         allRolesExist = false;
-                        interaction.editReply({
-                            content: `${interaction.reply.content}\n The role ${role.name} in the category ${record[2]}`,
-                        })
+                        return;
                     }
-                    return role;
-                }
-            )
-        })
+                    return new StringSelectMenuOptionBuilder()
+                        .setValue(role.id)
+                        .setLabel(role.name)
+                    }
+                ).filter(e => e != null))
+        ]))
     })
-    if (!allRolesExist) {
-        return [];
+    if (!allRolesExist)
+        return null;
+    if (!components.length) {
+        interaction.followUp({
+            content: "It looked like there was no CSV data" +
+                " (Or the file format was wrong.):" +
+                " \n ```\n" +  csvString + "\n```",
+            flags: MessageFlags.Ephemeral
+        })
     }
-    return categories;
-}
 
-function templateToActionRows() {
+    return components;
 
 }
 
